23e7ef5b0da4aaa6fb8fe7a34fe0aa0606ff0448
whitefei <1109091846@qq.com>
2017-04-26 23:55:16 -0500

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,3 +1,10 @@
+/*
+Everyday I remind myself that my inner and outer life are based on the labors of other men, living and dead, and I must exert myself 
+in order to give in the same measure as I have received and I am still rece
+                                --Albert Ainstein
+*/
+
+
 #include<iostream>
 using namespace std;
 
